aea21e5fb7ea782e19dc66febaf88b05cb74549c
younggglcy <younggglcy@gmail.com>
2025-10-15 01:51:14 +0800

    feat: enable ctrl + c to kill bumpp process

--- scripts/release.ts
+++ scripts/release.ts
@@ -1,4 +1,5 @@
-import { spawnSync } from 'node:child_process'
+import type { SpawnOptions, SpawnSyncOptions } from 'node:child_process'
+import { spawn, spawnSync } from 'node:child_process'
 import { existsSync } from 'node:fs'
 import { resolve } from 'node:path'
 import process from 'node:process'
@@ -7,43 +8,93 @@ import { fileURLToPath } from 'node:url'
 const __dirname = fileURLToPath(new URL('.', import.meta.url))
 const ROOT_DIR = resolve(__dirname, '..')
 
+const ac = new AbortController()
+
+process.on('SIGINT', () => {
+  console.log('!!! SIGINT !!!')
+  ac.abort()
+  process.exit(1)
+})
+process.on('uncaughtException', errorHandler)
+process.on('unhandledRejection', errorHandler)
+
 release()
 
-function release() {
+function errorHandler(err: Error) {
+  console.error('Error during release:', err)
+  process.exit(1)
+}
+
+function doSpawnSync(command: string, options?: SpawnSyncOptions) {
+  if (ac.signal.aborted) {
+    console.log('Aborted, exit.')
+    process.exit(1)
+  }
+  return spawnSync(
+    command,
+    {
+      stdio: 'inherit',
+      cwd: ROOT_DIR,
+      shell: true,
+      signal: ac.signal,
+      ...options,
+    },
+  )
+}
+
+async function doSpawn(command: string, options?: SpawnOptions) {
+  if (ac.signal.aborted) {
+    console.log('Aborted, exit.')
+    process.exit(1)
+  }
+  const child = spawn(
+    command,
+    {
+      stdio: 'inherit',
+      cwd: ROOT_DIR,
+      shell: true,
+      signal: ac.signal,
+      ...options,
+    },
+  )
+
+  return new Promise<void>((resolve, reject) => {
+    child.on('error', (err) => {
+      reject(err)
+    })
+    child.on('exit', (code, singal) => {
+      if (code === 0)
+        resolve()
+      else
+        reject(new Error(`Process exited with code ${code}, singal ${singal}`))
+    })
+  })
+}
+
+async function release() {
   console.log('Preparing package for release...')
 
   if (!existsSync(resolve(ROOT_DIR, 'CHANGELOG.md'))) {
     console.log('No CHANGELOG.md found, creating one...')
-    spawnSync(
-      `pnpm exec conventional-changelog -p angular -r 0`,
-      { stdio: 'inherit', cwd: ROOT_DIR, shell: true },
-    )
+    doSpawnSync(`pnpm exec conventional-changelog -p angular -r 0`)
   }
   else {
     console.log('Editing CHANGELOG.md...')
-    spawnSync(
-      `pnpm exec conventional-changelog -p angular`,
-      { stdio: 'inherit', cwd: ROOT_DIR, shell: true },
-    )
+    doSpawnSync(`pnpm exec conventional-changelog -p angular`)
   }
 
   console.log('Bumpping version...')
-  spawnSync(
-    `pnpm exec bumpp -p false`,
-    { stdio: 'inherit', cwd: ROOT_DIR, shell: true },
-  )
+  await doSpawn(`pnpm exec bumpp -p false`, { killSignal: 'SIGINT' })
+    .catch((err) => {
+      console.error('Error during bumpp:', err)
+      process.exit(1)
+    })
 
   console.log('Commit --amend --no-edit for CHANGELOG.md')
-  spawnSync(
-    `git add CHANGELOG.md && git commit --amend --no-edit`,
-    { stdio: 'inherit', cwd: ROOT_DIR, shell: true },
-  )
+  doSpawnSync(`git add CHANGELOG.md && git commit --amend --no-edit`)
 
   console.log('Pushing to remote...')
-  spawnSync(
-    `git push && git push --tags`,
-    { stdio: 'inherit', cwd: ROOT_DIR, shell: true },
-  )
+  doSpawnSync(`git push && git push --tags`)
 
   console.log('Release done.')
   process.exit(0)
