01ad9e8e187c1534823e802a20ac3c855fd252f7
younggglcy <younggglcy@gmail.com>
2025-10-15 02:03:52 +0800

    fix: make release script work

--- scripts/release.ts
+++ scripts/release.ts
@@ -1,9 +1,10 @@
-import type { SpawnOptions, SpawnSyncOptions } from 'node:child_process'
-import { spawn, spawnSync } from 'node:child_process'
+import type { SpawnSyncOptions } from 'node:child_process'
+import { spawnSync } from 'node:child_process'
 import { existsSync } from 'node:fs'
 import { resolve } from 'node:path'
 import process from 'node:process'
 import { fileURLToPath } from 'node:url'
+import { versionBump } from 'bumpp'
 
 const __dirname = fileURLToPath(new URL('.', import.meta.url))
 const ROOT_DIR = resolve(__dirname, '..')
@@ -42,35 +43,6 @@ function doSpawnSync(command: string, options?: SpawnSyncOptions) {
   )
 }
 
-async function doSpawn(command: string, options?: SpawnOptions) {
-  if (ac.signal.aborted) {
-    console.log('Aborted, exit.')
-    process.exit(1)
-  }
-  const child = spawn(
-    command,
-    {
-      stdio: 'inherit',
-      cwd: ROOT_DIR,
-      shell: true,
-      signal: ac.signal,
-      ...options,
-    },
-  )
-
-  return new Promise<void>((resolve, reject) => {
-    child.on('error', (err) => {
-      reject(err)
-    })
-    child.on('exit', (code, singal) => {
-      if (code === 0)
-        resolve()
-      else
-        reject(new Error(`Process exited with code ${code}, singal ${singal}`))
-    })
-  })
-}
-
 async function release() {
   console.log('Preparing package for release...')
 
@@ -84,14 +56,19 @@ async function release() {
   }
 
   console.log('Bumpping version...')
-  await doSpawn(`pnpm exec bumpp -p false`, { killSignal: 'SIGINT' })
-    .catch((err) => {
-      console.error('Error during bumpp:', err)
-      process.exit(1)
-    })
+  const bumpRes = await versionBump({
+    push: false,
+    tag: false,
+    commit: false,
+  })
 
-  console.log('Commit --amend --no-edit for CHANGELOG.md')
-  doSpawnSync(`git add CHANGELOG.md && git commit --amend --no-edit`)
+  console.log('Commit CHANGELOG.md && package.json...')
+
+  doSpawnSync(`git stash push`)
+  doSpawnSync(`git add CHANGELOG.md packages.json`)
+  doSpawnSync(`git commit --message "chore: release v${bumpRes.newVersion}"`)
+  doSpawnSync(`git tag v${bumpRes.newVersion} -m "chore: release v${bumpRes.newVersion}"`)
+  doSpawnSync(`git stash pop`)
 
   console.log('Pushing to remote...')
   doSpawnSync(`git push && git push --tags`)
